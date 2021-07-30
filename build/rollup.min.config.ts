b6f6e3ef7509db1d413f170f0479b87c1bba64bb
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-07-31 03:33:24 +0530

    refactor!: use rollup to bundle

---
+++ build/rollup.min.config.ts
@@ -0,0 +1,72 @@
+import alias from '@rollup/plugin-alias';
+import babel from '@rollup/plugin-babel';
+import beep from '@rollup/plugin-beep';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import sucrase from '@rollup/plugin-sucrase';
+import scss from 'rollup-plugin-scss';
+import { terser } from 'rollup-plugin-terser';
+import pkg from '../package.json';
+
+const extensions = ['.js', '.ts', '.vue'];
+
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * ${pkg.description}
+ * (c) 2021 ${pkg.author.name}<${pkg.author.email}>
+ * Released under the ${pkg.license} License
+ */
+`;
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/dash-shaka-clappr-playback.min.js',
+    format: 'umd',
+    name: 'VClappr',
+    exports: 'named',
+    strict: true,
+    sourcemap: true,
+    banner,
+    globals: {
+      '@clappr/core': 'core',
+      'shaka-player': 'shaka',
+    },
+  },
+  plugins: [
+    alias({
+      entries: {
+        Clappr: '@clappr/core/src/main.js',
+      },
+    }),
+    resolve({
+      extensions,
+      browser: true,
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
+    commonjs({
+      extensions,
+      include: 'node_modules',
+      exclude: 'src/**',
+    }),
+    scss({
+      output: 'dist/dash-shaka-clappr-playback.min.css',
+      outputStyle: 'compressed',
+    }),
+    sucrase({
+      exclude: ['node_modules/**'],
+      transforms: ['typescript'],
+    }),
+    terser({
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    }),
+    beep(),
+  ],
+  external: ['@clappr/core', 'shaka-player'],
+};
---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{js,ts}': 'npm run lint:js',
+  '*.{css}': 'npm run lint:css',
+};
